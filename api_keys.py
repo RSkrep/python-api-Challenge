3c88313bdc4a69ed23c4feaf5ab9c4a5d33c46f1
RSkrep <148006370+RSkrep@users.noreply.github.com>
2023-12-07 15:48:46 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-*weather_api_key = "aff19b2e9f6c2991174558d542411bc1"
+weather_api_key = "aff19b2e9f6c2991174558d542411bc1"
 
 # Geoapify API Key
-*geoapify_key = "7de19401ec3947d0809c8c121b052d29"
+geoapify_key = "7de19401ec3947d0809c8c121b052d29"
